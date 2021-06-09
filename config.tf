bc2086f8bbf4aa69ce392a50ec92031d016656d5
Michael Ethridge <methridge@hashicorp.com>
2021-06-09 08:02:54 -0600

    Merging config
    Signed-off-by: Michael Ethridge <methridge@hashicorp.com>

--- config.tf
+++ config.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.5"
+    }
+  }
+  required_version = ">= 0.15"
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "methridge"
-
     workspaces {
       name = "me-gcp-sandbox-network"
     }
